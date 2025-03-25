5f9a917f84ef0970b62a50a21462244b9bf0463b
Borui.Lin <105506491+hakuei0115@users.noreply.github.com>
2025-03-25 15:03:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
-
-from flask import request, abort
-from linebot import  LineBotApi, WebhookHandler
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import openai
 import os
+
+app = Flask(__name__)
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
@@ -26,21 +25,25 @@ def callback():
 
 @handler1.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    text1=event.message.text
-    response = openai.ChatCompletion.create(
-        messages=[
-            {"role": "user", "content": text1}
-        ],
-        model="gpt-3.5-turbo-0125",
-        temperature = 0.5,
-    )
+    global counter
+    text1 = event.message.text
     try:
+        response = openai.ChatCompletion.create(
+            messages=[
+                {"role": "user", "content": text1}
+            ],
+            model="gpt-3.5-turbo-0125",
+            temperature=0.5,
+        )
         ret = response['choices'][0]['message']['content'].strip()
         counter += 1
     except:
         ret = '發生錯誤！'
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=ret))
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=f"共傳了 {counter} 則訊息."
+
+    line_bot_api.reply_message(event.reply_token, [
+        TextSendMessage(text=ret),
+        TextSendMessage(text=f"共傳了 {counter} 則訊息。")
+    ])
 
 if __name__ == '__main__':
     app.run()
