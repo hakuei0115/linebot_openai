8be9fea36ff240add95f15703b7972ada8ec9541
Borui.Lin <105506491+hakuei0115@users.noreply.github.com>
2025-03-25 14:59:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 handler1 = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
+counter = 0
+
 @app.route('/callback', methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
@@ -34,9 +36,11 @@ def handle_message(event):
     )
     try:
         ret = response['choices'][0]['message']['content'].strip()
+        counter += 1
     except:
         ret = '發生錯誤！'
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=ret))
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=f"共傳了 {counter} 則訊息."
 
 if __name__ == '__main__':
     app.run()
